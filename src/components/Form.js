2619938964c8a97f086ef7fdd2e7119d500f90ec
steve-ux <exe.universal@gmail.com>
2022-04-13 13:01:05 -0300

    Se arregló bug del formulario de contacto

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,21 +4,19 @@ function Form() {
   return (
     <div className="formulario">
       <div class="login-box">
-        <form
-          action="https://formsubmit.co/exe.universal@gmail.com"
-          method="POST"
-        >
+        <form action="https://formsubmit.co/exe.universal@gmail.com" method="POST">
           <p>ENVIÁ TU CONSULTA</p>
           <div class="user-box">
-            <input required type="text" placeholder="Nombre o apodo" />
+            <input required type="text" name="Nombre" placeholder="Nombre o apodo" />
             <br />
           </div>
           <div class="user-box">
-            <input required type="email" placeholder="Email" />
+            <input required type="email" name="Correo" placeholder="Email" />
             <br />
             <textarea
               required
               type="textarea"
+              name="Mensaje"
               minLength="4"
               title="Mensaje mínimo de 4 caracteres"
               placeholder="Tu mensaje"
